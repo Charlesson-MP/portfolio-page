516b9624aeeb5b9402dcf488003deb7ecbe84bd4
Charlesson Mendes Pereira <86690606+Charlesson-MP@users.noreply.github.com>
2025-07-26 00:15:21 -0300

    feat(about): finaliza estrutura e estilo da seção sobre mim

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -4,7 +4,6 @@ const btnHome = document.getElementById("btnHome");
 const btnAbout = document.getElementById("btnAbout");
 const btnSkills = document.getElementById("btnSkills");
 const btnProjects = document.getElementById("btnProjects");
-const btnContact = document.getElementById("btnContact");
 
 function abrirFecharMenu() {
     menuList.classList.toggle("aberto");
@@ -14,7 +13,6 @@ btnMenu.addEventListener("click", abrirFecharMenu);
 btnHome.addEventListener("click", abrirFecharMenu);
 btnAbout.addEventListener("click", abrirFecharMenu);
 btnSkills.addEventListener("click", abrirFecharMenu);
-btnContact.addEventListener("click", abrirFecharMenu);
 
 document.addEventListener("click", (evt) => {
     const clickForaDoMenu = getComputedStyle(menuList).display === "flex" && !menuList.contains(evt.target) && evt.target !== btnMenu;
