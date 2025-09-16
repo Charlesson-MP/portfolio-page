ba09c3cfe23f2aae84ba71deed5ff13261d92507
Charlesson Mendes Pereira <86690606+Charlesson-MP@users.noreply.github.com>
2025-09-16 20:00:10 -0300

    feat: implementar renderização dinâmica dos projetos a partir do JSON

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -4,6 +4,12 @@ const btnHome = document.getElementById("btnHome");
 const btnAbout = document.getElementById("btnAbout");
 const btnSkills = document.getElementById("btnSkills");
 const btnProjects = document.getElementById("btnProjects");
+const projecsBoard = document.querySelector('.projects-board');
+const btnPrev = document.querySelector('.btnPrev');
+const btnNext = document.querySelector('.btnNext');
+const container = document.querySelector('.container-projects');
+const track = document.querySelector('.projects-board');
+let currentProject = 0;
 
 function abrirFecharMenu() {
     menuList.classList.toggle("aberto");
@@ -21,18 +27,89 @@ document.addEventListener("click", (evt) => {
         abrirFecharMenu()
 });
 
-const btnPrev = document.querySelector('.btnPrev');
-const btnNext = document.querySelector('.btnNext');
-const container = document.querySelector('.container-projects');
-const track = document.querySelector('.projects-board');
-let currentProject = 0;
+async function loadProjects() {
+    const response = await fetch('./src/data/projects.json');
+    const data = await response.json();
+
+    return data;
+}
+
+function createCardProject(coverPath, altImg, title, description, linkPage, linkRepository) {
+    const projectCard = document.createElement('div');
+    projectCard.classList.add('project');
+
+    const containerImgProject = document.createElement('div');
+    containerImgProject.classList.add('img-project');
+
+    const imgProject = document.createElement('img');
+    imgProject.src = coverPath;
+    imgProject.alt = altImg;
+
+    containerImgProject.appendChild(imgProject);
+
+    const containerInfo = document.createElement('div');
+    containerInfo.classList.add('text-card-project');
+
+    const projectTitle = document.createElement('h3');
+    projectTitle.textContent = title;
+
+    const projectDescription = document.createElement('p');
+    projectDescription.textContent = description;
+
+    const linksContainer = document.createElement('div');
+    linksContainer.classList.add('links');
+
+    const linkProjectPage = document.createElement('a');
+    linkProjectPage.textContent = 'Página';
+    linkProjectPage.href = linkPage;
+
+    const linkRepositoryPage = document.createElement('a');
+    linkRepositoryPage.textContent = 'Repositório';
+    linkRepositoryPage.href = linkRepository;
+
+    linkProjectPage.target = linkRepositoryPage.target = '_blank';
+
+    linksContainer.append(linkProjectPage, linkRepositoryPage);
+
+    containerInfo.append(projectTitle, projectDescription, linksContainer);
+
+    projectCard.append(containerImgProject, containerInfo);
+
+    return projectCard;
+}
+
+async function showProjects() {
+    try {
+        const data = await loadProjects();
+
+        data.forEach(project => {
+            const cardProject = createCardProject(project.cover_path, project.alt_img, project.title, project.description, project.link_page, project.link_repository_page);
+            projecsBoard.appendChild(cardProject);
+        });
+    }catch(error) {
+        console.error('Erro ao mostrar projetos:', error);
+    }
+
+    updateCarousel();
+}
+
+showProjects();
 
 function updateLimit() {
-    const containerWidth = container.offsetWidth;
-    const cardWidth = track.children[0].offsetWidth + 32;
+    let limit;
 
-    const visibleCards = Math.floor(containerWidth / cardWidth);
-    return track.children.length - visibleCards;
+    if(track.children.length) {
+        const containerWidth = container.offsetWidth;
+        const cardWidth = track.children[0].offsetWidth + 32;
+
+        const visibleCards = Math.floor(containerWidth / cardWidth);
+
+        limit = track.children.length - visibleCards;
+    }else {
+        limit = 0;
+    }
+    
+    return limit;
 }
 
 btnPrev.addEventListener('click', () => {
@@ -63,7 +140,5 @@ function updateCarousel() {
     btnNext.disabled = currentProject === limitCard;
     btnNext.classList.toggle('disable', btnNext.disabled);
 }
-
-updateCarousel();
 
 window.addEventListener('resize', updateCarousel);
